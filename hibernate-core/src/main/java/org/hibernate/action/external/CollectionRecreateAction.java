31b39d9d497592808e8c7c4404969ef142cea3c0
jim53214066 <48121013+jim53214066@users.noreply.github.com>
2024-05-28 15:28:48 +0800

    update by n8n

--- hibernate-core/src/main/java/org/hibernate/action/external/CollectionRecreateAction.java
+++ hibernate-core/src/main/java/org/hibernate/action/external/CollectionRecreateAction.java
@@ -1,3 +1,4 @@
+```java
 /*
  * Hibernate, Relational Persistence for Idiomatic Java
  *
@@ -23,56 +24,57 @@ import org.hibernate.stat.spi.StatisticsImplementor;
  */
 public final class CollectionRecreateAction extends CollectionAction {
 
-	/**
-	 * Constructs a CollectionRecreateAction
-	 *  @param collection The collection being recreated
-	 * @param persister The collection persister
-	 * @param id The collection key
-	 * @param session The session
-	 */
-	public CollectionRecreateAction(
-			final PersistentCollection<?> collection,
-			final CollectionPersister persister,
-			final Object id,
-			final EventSource session) {
-		super( persister, collection, id, session );
-	}
+    /**
+     * Constructs a CollectionRecreateAction
+     *  @param collection The collection being recreated
+     * @param persister The collection persister
+     * @param id The collection key
+     * @param session The session
+     */
+    public CollectionRecreateAction(
+            final PersistentCollection<?> collection,
+            final CollectionPersister persister,
+            final Object id,
+            final EventSource session) {
+        super(persister, collection, id, session);
+    }
 
-	@Override
-	public void execute() throws HibernateException {
-		// this method is called when a new non-null collection is persisted
-		// or when an existing (non-null) collection is moved to a new owner
-		final PersistentCollection<?> collection = getCollection();
-		preRecreate();
-		final SharedSessionContractImplementor session = getSession();
-		getPersister().recreate( collection, getKey(), session);
-		session.getPersistenceContextInternal().getCollectionEntry( collection ).afterAction( collection );
-		evict();
-		postRecreate();
+    @Override
+    public void execute() throws HibernateException {
+        // this method is called when a new non-null collection is persisted
+        // or when an existing (non-null) collection is moved to a new owner
+        final PersistentCollection<?> collection = getCollection();
+        preRecreate();
+        final SharedSessionContractImplementor session = getSession();
+        getPersister().recreate(collection, getKey(), session);
+        session.getPersistenceContextInternal().getCollectionEntry(collection).afterAction(collection);
+        evict();
+        postRecreate();
 
-		final StatisticsImplementor statistics = session.getFactory().getStatistics();
-		if ( statistics.isStatisticsEnabled() ) {
-			statistics.recreateCollection( getPersister().getRole() );
-		}
-	}
+        final StatisticsImplementor statistics = session.getFactory().getStatistics();
+        if (statistics.isStatisticsEnabled()) {
+            statistics.recreateCollection(getPersister().getRole());
+        }
+    }
 
-	private void preRecreate() {
-		getFastSessionServices().eventListenerGroup_PRE_COLLECTION_RECREATE
-				.fireLazyEventOnEachListener( this::newPreCollectionRecreateEvent,
-						PreCollectionRecreateEventListener::onPreRecreateCollection );
-	}
+    private void preRecreate() {
+        getFastSessionServices().eventListenerGroup_PRE_COLLECTION_RECREATE
+                .fireLazyEventOnEachListener(this::newPreCollectionRecreateEvent,
+                        PreCollectionRecreateEventListener::onPreRecreateCollection);
+    }
 
-	private PreCollectionRecreateEvent newPreCollectionRecreateEvent() {
-		return new PreCollectionRecreateEvent( getPersister(), getCollection(), eventSource() );
-	}
+    private PreCollectionRecreateEvent newPreCollectionRecreateEvent() {
+        return new PreCollectionRecreateEvent(getPersister(), getCollection(), eventSource());
+    }
 
-	private void postRecreate() {
-		getFastSessionServices().eventListenerGroup_POST_COLLECTION_RECREATE
-				.fireLazyEventOnEachListener( this::newPostCollectionRecreateEvent,
-						PostCollectionRecreateEventListener::onPostRecreateCollection );
-	}
+    private void postRecreate() {
+        getFastSessionServices().eventListenerGroup_POST_COLLECTION_RECREATE
+                .fireLazyEventOnEachListener(this::newPostCollectionRecreateEvent,
+                        PostCollectionRecreateEventListener::onPostRecreateCollection);
+    }
 
-	private PostCollectionRecreateEvent newPostCollectionRecreateEvent() {
-		return new PostCollectionRecreateEvent( getPersister(), getCollection(), eventSource() );
-	}
+    private PostCollectionRecreateEvent newPostCollectionRecreateEvent() {
+        return new PostCollectionRecreateEvent(getPersister(), getCollection(), eventSource());
+    }
 }
+```
